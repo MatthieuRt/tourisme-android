9fe07e26b100861ba88dd5b4a30f29f54ecbf78a
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-07-31 22:23:15 +0300

    Mode nuit

--- app/src/main/java/com/example/roadtomadagascar/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/roadtomadagascar/Activities/SettingsActivity.java
@@ -1,16 +1,36 @@
 package com.example.roadtomadagascar.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.example.roadtomadagascar.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    CheckBox checkBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        checkBox = findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                int currentNightMode = AppCompatDelegate.getDefaultNightMode();
+                int newNightMode = b ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO;
+                if (currentNightMode != newNightMode) {
+                    AppCompatDelegate.setDefaultNightMode(newNightMode);
+                    getDelegate().applyDayNight();
+                    recreate();
+                }
+            }
+        });
+
     }
 }
