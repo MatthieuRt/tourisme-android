deb444ad34834051d42281696af69452ab116354
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-08-01 21:10:39 +0300

    ajout d adapter
    - hotel

--- app/src/main/java/com/example/roadtomadagascar/Adapters/HotelAdapter.java
+++ app/src/main/java/com/example/roadtomadagascar/Adapters/HotelAdapter.java
@@ -1,4 +1,75 @@
 package com.example.roadtomadagascar.Adapters;
 
-public class HotelAdapter {
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners;
+import com.example.roadtomadagascar.Activities.DetailActivity;
+import com.example.roadtomadagascar.Activities.ListActivity;
+import com.example.roadtomadagascar.Domains.CategoryDomain;
+import com.example.roadtomadagascar.Domains.HotelDomain;
+import com.example.roadtomadagascar.R;
+
+import java.util.ArrayList;
+
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>{
+
+    ArrayList<HotelDomain> items;
+
+    public HotelAdapter(ArrayList<HotelDomain> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public HotelAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_hotel,parent,false);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HotelAdapter.ViewHolder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        holder.locationTxt.setText(items.get(position).getLocation());
+        holder.scoreTxt.setText(""+items.get(position).getScore());
+
+        int drawableResId=holder.itemView.getResources().getIdentifier(items.get(position).getPic(),"drawable",holder.itemView.getContext().getPackageName());
+        Glide.with(holder.itemView.getContext())
+                .load(drawableResId)
+                .transform(new CenterCrop(), new GranularRoundedCorners(40,40,40,40))
+                .into(holder.pic);
+
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(holder.itemView.getContext(), DetailActivity.class);
+            holder.itemView.getContext().startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView titleTxt, locationTxt,scoreTxt;
+        ImageView pic,wifiImg;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            locationTxt = itemView.findViewById(R.id.locationTxt);
+            scoreTxt = itemView.findViewById(R.id.scoreTxt);
+            pic=itemView.findViewById(R.id.picImg);
+            wifiImg=itemView.findViewById(R.id.wifiImg);
+        }
+    }
 }
