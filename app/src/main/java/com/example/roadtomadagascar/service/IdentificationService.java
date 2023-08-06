789ed88797414fd2cf70e654af03904203214acb
NainaMatthieu <89472863+NainaMatthieu@users.noreply.github.com>
2023-08-06 11:17:46 +0300

    Inscription service

--- app/src/main/java/com/example/roadtomadagascar/service/IdentificationService.java
+++ app/src/main/java/com/example/roadtomadagascar/service/IdentificationService.java
@@ -40,7 +40,7 @@ public class IdentificationService {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
                                 // Login successful
-                                JSONObject userData = jsonResponse.getJSONObject("data");
+                                JSONObject userData = jsonResponse.getJSONObject("success");
                                 String userId = userData.getString("_id");
                                 String username = userData.getString("username");
                                 // Save user data to session
@@ -88,6 +88,83 @@ public class IdentificationService {
     }
 
     public interface LoginCallback {
+        void onSuccess();
+        void onError(String errorMessage);
+    }
+
+    public void inscription(String mail,String nom,String prenom,String password, Context context,final LogupCallback logupCallback){
+        SessionUser sessionUser = SessionUser.getSessionUser();
+
+        String url = "https://back-tourisme-git-main-matthieurt.vercel.app/user/logup"; // Remplacez par l'URL de votre API
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(context);
+        // Create JSON object for the logup
+        JSONObject jsonLogup = new JSONObject();
+        try {
+            jsonLogup.put("mail", mail);
+            jsonLogup.put("nom", nom);
+            jsonLogup.put("prenom", prenom);
+            jsonLogup.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Process the response from the server
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            // Login successful
+                            JSONObject userData = jsonResponse.getJSONObject("success");
+                            String userId = userData.getString("_id");
+                            String username = userData.getString("username");
+                            // Save user data to session
+                            sessionUser.setIdUser(userId);
+                            sessionUser.setUsername(username);
+                            // Callback to indicate successful login
+                            SessionUser.saveSession(context, userId);
+                            logupCallback.onSuccess();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            // Callback to indicate login failure with a general error message
+                            logupCallback.onError("An error occurred during login.");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Check if the error contains a network response
+                if (error.networkResponse != null) {
+                    int statusCode = error.networkResponse.statusCode;
+                    // Callback to indicate login failure with the error message and status code
+                    logupCallback.onError("Login failed with status code: " + statusCode);
+                    sessionUser.setErroMessage(new String(error.networkResponse.data));
+
+                } else {
+                    // Callback to indicate login failure with a general error message
+                    logupCallback.onError("An error occurred during login.");
+                }
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public byte[] getBody() {
+                return jsonLogup.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+
+    public interface LogupCallback {
         void onSuccess();
         void onError(String errorMessage);
     }
