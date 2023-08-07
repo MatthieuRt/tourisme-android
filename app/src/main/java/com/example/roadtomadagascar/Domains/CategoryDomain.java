1e3933da5e7c58971308494a9b538c3fed6b1ba7
NainaMatthieu <89472863+NainaMatthieu@users.noreply.github.com>
2023-08-07 14:12:24 +0300

    Update CategoryDomain.java

--- app/src/main/java/com/example/roadtomadagascar/Domains/CategoryDomain.java
+++ app/src/main/java/com/example/roadtomadagascar/Domains/CategoryDomain.java
@@ -3,12 +3,27 @@ package com.example.roadtomadagascar.Domains;
 import java.io.Serializable;
 
 public class CategoryDomain implements Serializable {
+    private String id;
     private String title;
     private String picPath;
 
     public CategoryDomain(String title, String picPath) {
         this.title = title;
         this.picPath = picPath;
+    }
+
+    public CategoryDomain(String id, String title, String picPath) {
+        this.id = id;
+        this.title = title;
+        this.picPath = picPath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
